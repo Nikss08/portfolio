7b6bd71dad6a440d77b1d5a71ed29bbd04de0d83
Nikunj Mistry <niiksss810@gmail.com>
2026-06-30 19:34:21 +0530

    error fixing 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 
