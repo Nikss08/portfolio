f2f15fa101660656f7165b071066a910668b7b0c
Nikunj Mistry <niiksss810@gmail.com>
2026-06-30 19:25:49 +0530

    error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
